0e03aff47a70f2b09c4942f066876760c9ddf14a
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-11-09 13:13:29 -0800

    contains duplicates notes

--- leetcode/arrays_hasing/containsDuplicates.js
+++ leetcode/arrays_hasing/containsDuplicates.js
@@ -2,3 +2,14 @@
  * Given an integer array nums, return true if any value appears at least twice in the array,
  * and return false if every element is distinct.
  */
+
+var containsDuplicate = function (nums) {
+  let set = new Set();
+  for (let num of nums) {
+    if (set.has(num)) {
+      return true;
+    }
+    set.add(num);
+  }
+  return false;
+};
