5747a8f157ff2d39a8e9ebb94380dd646b1982f5
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-11-12 13:21:56 -0800

    valid anagram

--- leetcode/arrays_hasing/validAnagram.js
+++ leetcode/arrays_hasing/validAnagram.js
@@ -1,2 +1,26 @@
 // 242. Valid Anagram
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length != t.length) {
+    return false;
+  }
+
+  let map = new Map();
+  for (let char of s) {
+    map.set(char, (map.get(char) || 0) + 1);
+  }
+  for (let char of t) {
+    if (!map.get(char)) {
+      return false;
+    } else {
+      map.set(char, map.get(char) - 1);
+    }
+  }
+  return true;
+};
