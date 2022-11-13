1c8062638e2d8d1f82a888cdd5ac7786d2439658
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-11-13 13:29:30 -0800

    valid anagram added comments

--- leetcode/arrays_hasing/validAnagram.js
+++ leetcode/arrays_hasing/validAnagram.js
@@ -7,18 +7,23 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-  if (s.length != t.length) {
+  //if the two strings are not the same length we know they are not anagrams.
+  if (s.length !== t.length) {
     return false;
   }
-
+  //create a map to hold the values from the strings
   let map = new Map();
+  //go through the first array of characters and add its characters to the map and increment if they exist
   for (let char of s) {
     map.set(char, (map.get(char) || 0) + 1);
   }
+  //go through the second array of characters
   for (let char of t) {
+    //if the char does not exist in the map return false;
     if (!map.get(char)) {
       return false;
     } else {
+      //otherwise decrement the value of the char
       map.set(char, map.get(char) - 1);
     }
   }
