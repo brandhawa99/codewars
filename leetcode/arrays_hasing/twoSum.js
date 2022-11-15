d817aa919d2d21175c4fdbb9e77c1672b9500448
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-11-15 13:41:40 -0800

    two sum

--- leetcode/arrays_hasing/twoSum.js
+++ leetcode/arrays_hasing/twoSum.js
@@ -3,3 +3,20 @@
  * Given an array of integers nums and an integer target,
  * return indices of the two numbers such that they add up to target.
  */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let couple = target - nums[i];
+    if (map.get(couple) != null) {
+      return [map.get(couple), i];
+    }
+    map.set(nums[i], i);
+  }
+  return;
+};
