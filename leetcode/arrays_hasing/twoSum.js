76ebc823a1159aef19ad1fe892e9e59d648c3d23
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-11-16 13:48:33 -0800

    added comments to two sum

--- leetcode/arrays_hasing/twoSum.js
+++ leetcode/arrays_hasing/twoSum.js
@@ -10,12 +10,17 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+  // create a map to hold the values that are needed to added up to the target
   let map = new Map();
   for (let i = 0; i < nums.length; i++) {
+    //figure out what value is need to add up to the target
     let couple = target - nums[i];
+    //check if that value exists in the map
     if (map.get(couple) != null) {
+      //if it does get its index and the current index
       return [map.get(couple), i];
     }
+    //if the couple is not in the map add the current value to the map
     map.set(nums[i], i);
   }
   return;
