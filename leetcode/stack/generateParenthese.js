4c504afda0b676c44123adfcff11bcd2fd711c08
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-06-25 13:30:04 -0700

    generate parenthesis

--- leetcode/stack/generateParenthese.js
+++ leetcode/stack/generateParenthese.js
@@ -2,27 +2,34 @@
  * @param {number} n
  * @return {string[]}
  */
-var generateParenthesis = function(n) {
-    let stack = []; 
-    let res = []; 
-    const backtracking = (open , close) =>{
-        console.log(stack); 
-        if(open === close && open === n){
-            res.push(stack.join("")); 
-            return; 
+var generateParenthesis = function (n) {
+    /**
+     * only add open parenthesis if open < n
+     * only add closing parenthesis if closed < open 
+     * valid IFF open === close == n 
+     */
+
+    let stack = [];
+    let res = [];
+    const backtracking = (open, close) => {
+        if (open === close && open === n) {
+            res.push(stack.join(""));
+            return;
         }
-        if(open < n){
-            stack.push("("); 
+        if (open < n) {
+            stack.push("(");
             backtracking(open + 1, close);
-            stack.pop(); 
+            stack.pop();
         }
-        if(close < open){
-            stack.push(")"); 
+        if (close < open) {
+            stack.push(")");
             backtracking(open, close + 1)
-            stack.pop(); 
+            stack.pop();
         }
     }
-    backtracking(0,0); 
-    return res; 
+    backtracking(0, 0);
+    return res;
 
 };
+
+generateParenthesis(3)
