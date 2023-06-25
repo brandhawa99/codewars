c1b55ce4d03e20006e1663a8459219ee04b7d4c0
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-06-24 23:52:29 -0700

    dailyTemperatures

--- leetcode/stack/dailyTemp.js
+++ leetcode/stack/dailyTemp.js
@@ -2,16 +2,16 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(T) {
-    let stack = []; 
-    let res = new Array(T.length).fill(0);  
-    for(let i = 0 ; i < T.length; i++){
-        while(stack.length >0 && T[i] > stack[stack.length -1][0]){
-           let val = stack.pop() 
-           res[val[1]] = (i - val[1])
-        }
-        stack.push([T[i], i])
+var dailyTemperatures = function (T) {
+    let stack = [];
+    let res = Array(T.length - 1).fill(0);
 
+    for (let i = 0; i < T.length; i++) {
+        while (stack.length && T[i] > stack[stack.length - 1].value) {
+            let val = stack.pop();
+            res[val.index] = i - val.index;
+        }
+        stack.push({ value: T[i], index: i })
     }
-    return res; 
+    return res;
 };
