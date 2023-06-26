8b137d939d093f6c3906bc6258f5e772e928dc15
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-06-26 12:53:11 -0700

    added comments

--- leetcode/stack/dailyTemp.js
+++ leetcode/stack/dailyTemp.js
@@ -3,9 +3,14 @@
  * @return {number[]}
  */
 var dailyTemperatures = function (T) {
+    // keep track of values read
     let stack = [];
+    // output array  
     let res = Array(T.length - 1).fill(0);
 
+    // iterate through Temperatures array 
+    // if stack has values less than current them update res array 
+    // push current value to stack 
     for (let i = 0; i < T.length; i++) {
         while (stack.length && T[i] > stack[stack.length - 1].value) {
             let val = stack.pop();
