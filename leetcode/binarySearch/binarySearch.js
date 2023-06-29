8250bc46905aee50b68656f4e113fe93f000c802
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-06-29 12:35:20 -0700

    binary search

--- leetcode/binarySearch/binarySearch.js
+++ leetcode/binarySearch/binarySearch.js
@@ -3,19 +3,25 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-    let [start, end] = [0, nums.length-1]
+var search = function (nums, target) {
 
-    while(start <= end ){
-        let mid = Math.floor((start+end)/2)
-        let val = nums[mid]
-        if(val === target){
-            return mid; 
-        }else if(target > val){
-            start = mid +1; 
-        } else if(target < val){
-            end = mid - 1; 
+    let l = 0;
+    let r = nums.length - 1;
+
+    // l <= r because if there is only one item in array it call still solve 
+    while (l <= r) {
+        let mid = l + Math.floor((r - l) / 2)
+        let midVal = nums[mid];
+
+        if (target > midVal) {
+            l = mid + 1;
+        } else if (target < midVal) {
+            r = mid - 1
+        } else {
+            return mid;
         }
     }
-    return -1; 
+    return -1;
 };
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9))
