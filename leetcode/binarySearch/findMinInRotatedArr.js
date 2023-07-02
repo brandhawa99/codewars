58af530cf0ae137e05fd3b89ac7670ce707fd6a7
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-07-01 17:58:14 -0700

    find min in rotated array

--- leetcode/binarySearch/findMinInRotatedArr.js
+++ leetcode/binarySearch/findMinInRotatedArr.js
@@ -2,19 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function(nums) {
-     let [start, end] = [0 , nums.length -1]; 
-     let min = Infinity
+var findMin = function (nums) {
 
-     while(start < end){
-        let mid  = Math.floor((start+end)/2)
-        min = Math.min(min, nums[mid]); 
-        if(nums[mid] > nums[end]){
-            start = mid + 1; 
-        }else{
-            end = mid -1; 
+    let l = 0;
+    let r = nums.length - 1;
+    let min = Infinity;
+
+    while (l <= r) {
+        // if left val is less than the right val we have found the minimum value 
+        if (nums[l] <= nums[r]) {
+            min = nums[l];
+            break;
         }
-     }
-     return Math.min(min,nums[start])
-    
+        let mid = l + Math.floor((r - l) / 2);
+        min = Math.min(nums[mid], min);
+        // if the left val is less than the mid val the numbers left of mid do not include the minimum value 
+        if (nums[l] <= nums[mid]) {
+            l = mid + 1
+        } else {
+            r = mid - 1;
+        }
+    }
+
+    return min
 };
