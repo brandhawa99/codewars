d8b471ad8f9984687f4ebc03e600e68c16d35315
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-08-27 11:19:16 -0700

    reorder list

--- leetcode/linked_list/reorderList.js
+++ leetcode/linked_list/reorderList.js
@@ -4,7 +4,14 @@ const reorderList = (head) => {
 }
 
 const findMid = (head) => {
+  let fast = head;
+  let slow = head;
 
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+  return slow;
 }
 
 const reverse = (head) => {
