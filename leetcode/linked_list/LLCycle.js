2b065444363d9b3a513fc3faf8b1bc601b6d5d60
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-09-07 11:12:44 -0700

    LL cycle

--- leetcode/linked_list/LLCycle.js
+++ leetcode/linked_list/LLCycle.js
@@ -4,16 +4,17 @@
  * @returns {boolean};
  */
 const hasCycle = (head) => {
-
   let fast = head;
   let slow = head;
 
+  // if loop exists fast pointer will meet with slow pointer at some point 
   while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+
     if (fast === slow) {
       return true;
     }
-    fast = fast.next.next;
-    slow = slow.next;
   }
   return false;
 }
