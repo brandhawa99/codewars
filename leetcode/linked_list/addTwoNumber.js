faced59e409c2414b5c9b7853e34c684c0bd4e9d
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-09-09 21:58:46 -0700

    addtwoNums

--- leetcode/linked_list/addTwoNumber.js
+++ leetcode/linked_list/addTwoNumber.js
@@ -6,5 +6,17 @@
 
 const addTwoNumbers = (l1, l2) => {
 
+
+  let remainder = 0;
+  let head = {}
+  let curr = head;
+
+  while (l1 || l2 || remainder) {
+    let num1 = l1?.val || 0;
+    let num2 = l2?.val || 0;
+
+
+
+  }
   return {}
 }
