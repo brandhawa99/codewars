9b93b065fc410e05b2f665eb22d7a0ef19402c91
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-09-18 23:03:51 -0700

    added methods

--- leetcode/linked_list/LRUCache.js
+++ leetcode/linked_list/LRUCache.js
@@ -10,7 +10,32 @@ var LRUCache = (capacity) => {
 }
 
 
+
 LRUCache.prototype.get = (key) => {
+
+  const hasKey = this.map.has(key);
+  if (!hasKey) return -1;
+
+  const node = this.map.get(key);
+  this.removeNode(node);
+  // move node to front
+
+
+  return node.value;
+
+}
+
+LRUCache.prototype.removeNode = function (node) {
+
+  node.next.prev = node.prev;
+  node.prev.next = node.next;
+
+}
+
+LRUCache.prototype.moveToFront = function () {
+
+  const [prev, next] = [this.tail.prev, this.tail];
+
 
 }
 
