5ee049603cb4b46499825b426f27ff44d9b7572b
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-09-21 09:59:07 -0700

    max depth of binary tree

---
+++ leetcode/trees/maximumDepthOfBT.js
@@ -0,0 +1,13 @@
+
+/**
+ * 
+ * @param {TreeNode} root 
+ * @returns {number} 
+ */
+const maxDepth = (root) => {
+  if (root == null) {
+    return 0;
+  }
+
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+}
