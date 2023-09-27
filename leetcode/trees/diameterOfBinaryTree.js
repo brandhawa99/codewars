cc0fe38596e4e03f3d505e4cf272f86fb754e03b
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-09-27 09:34:03 -0700

    copmleted diameter of binary tree

--- leetcode/trees/diameterOfBinaryTree.js
+++ leetcode/trees/diameterOfBinaryTree.js
@@ -2,7 +2,18 @@ const diameterOfBinaryTree = (root) => {
   let res = [0];
 
   const dfs = (root) => {
+    if (root === null) {
+      return -1;
+    }
+
+    const left = dfs(root.left);
+    const right = dfs(root.right);
+
+    res[0] = Math.max(res[0], left + right + 2);
+
+    return 1 + Math.max(left, right);
 
   }
+  dfs(root);
   return res[0]
 }
